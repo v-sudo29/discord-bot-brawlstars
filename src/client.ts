25b525140cb0c8f5a9dcf864b0406a0db9195352
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-12-13 10:14:15 -0500

    Create server, add interaction, add response

---
+++ src/client.ts
@@ -0,0 +1,157 @@
+import { Client, GatewayIntentBits, Routes, EmbedBuilder } from 'discord.js'
+import { REST } from 'discord.js'
+const axios = require('axios');
+require('dotenv').config()
+
+const CLIENT_TOKEN = process.env.CLIENT_TOKEN ?? ''
+const CLIENT_ID = process.env.CLIENT_ID ?? ''
+
+const VI_PLAYER_ID = process.env.VI_PLAYER_ID ?? ''
+const AMANDA_PLAYER_ID = process.env.AMANDA_PLAYER_ID ?? ''
+const RYAN_PLAYER_ID = process.env.RYAN_PLAYER_ID ?? ''
+const ANDY_PLAYER_ID = process.env.ANDY_PLAYER_ID ?? ''
+const ALAN_PLAYER_ID = process.env.ALAN_PLAYER_ID ?? ''
+const ERIK_PLAYER_ID = process.env.ERIK_PLAYER_ID ?? ''
+
+export const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+})
+
+async function registerSlashCommand() {
+  const commands = [
+    {
+      name: "trophies",
+      description: "List player rankings",
+    },
+  ]
+
+  const rest = new REST().setToken(CLIENT_TOKEN)
+
+  try {
+    console.log("Registering commands [/]")
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands })
+    console.log("Registered commands! [/]")
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+client.on("ready", async () => {
+  console.log(`${client?.user?.username} is now connected to Discord`)
+  // client?.user?.setActivity(`Watching ${client.guilds.cache.size} servers!`)
+  client.user.setActivity('Brawlstars')
+  registerSlashCommand()
+})
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return
+  if (interaction.commandName === "trophies") {
+    
+    // inside a command, event listener, etc.
+    // const exampleEmbed = new EmbedBuilder()
+    // .setColor(0x0099FF)
+    // .setTitle('Some title')
+    // .setURL('https://discord.js.org/')
+    // .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+    // .setDescription('Some description here')
+    // .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+    // .addFields(
+    //   { name: 'Regular field title', value: 'Some value here' },
+    //   { name: '\u200B', value: '\u200B' },
+    //   { name: 'Inline field title', value: 'Some value here', inline: true },
+    //   { name: 'Inline field title', value: 'Some value here', inline: true },
+    // )
+    // .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+    // .setImage('https://i.imgur.com/AfFp7pu.png')
+    // .setTimestamp()
+    // .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+    // channel.send({ embeds: [exampleEmbed] });
+
+    let viTrophies = null
+    let amandaTrophies = null
+    let ryanTrophies = null
+    let andyTrophies = null
+    let alanTrophies = null
+    let erikTrophies = null
+    let finalReply = ''
+
+    // Fetch data from API
+    await axios.all([
+      axios(`http://localhost:3000/${VI_PLAYER_ID}`),
+      axios(`http://localhost:3000/${AMANDA_PLAYER_ID}`),
+      axios(`http://localhost:3000/${RYAN_PLAYER_ID}`),
+      axios(`http://localhost:3000/${ANDY_PLAYER_ID}`),
+      axios(`http://localhost:3000/${ALAN_PLAYER_ID}`),
+      axios(`http://localhost:3000/${ERIK_PLAYER_ID}`),
+    ])
+      .then(axios.spread((
+        viData,
+        amandaData,
+        ryanData,
+        andyData,
+        alanData,
+        erikData
+      ) => {
+        viTrophies = viData.data.trophies + ' --------Vi'
+        amandaTrophies = amandaData.data.trophies + ' --------Amanda'
+        ryanTrophies = ryanData.data.trophies + ' --------Ryan'
+        andyTrophies = andyData.data.trophies + ' --------Andy'
+        alanTrophies = alanData.data.trophies + ' --------Alan'
+        erikTrophies = erikData.data.trophies + ' --------Erik'
+
+        let playersArr = [
+          viTrophies,
+          amandaTrophies,
+          ryanTrophies,
+          andyTrophies,
+          alanTrophies,
+          erikTrophies
+        ]
+
+        // Sort trophies from highest to lowest
+        let sortedPlayers = playersArr.sort((a, b) => {
+          let playerA = parseInt(a.split(' ')[0])
+          let playerB = parseInt(b.split(' ')[0])
+
+          if (playerA > playerB) return -1
+          if (playerB > playerA) return 1
+          return 0
+        })
+
+        // Add rankings
+        for (let i = 0; i < sortedPlayers.length; i++) {
+          const playerName = sortedPlayers[i].split(' ')[1]
+          const convertedTrophiesNum = parseInt(sortedPlayers[i].split(' ')[0]).toLocaleString() 
+          sortedPlayers[i] = convertedTrophiesNum + playerName
+          sortedPlayers[i] = `${i + 1}. ${sortedPlayers[i]}`
+        }
+
+        // Convert to string
+        finalReply = sortedPlayers.join('\n')
+        console.log(finalReply)
+      }))
+
+      .catch(error => {
+        console.log(error)
+      })
+
+    const embed = new EmbedBuilder()
+      .setColor('Random')
+      .setTitle('Trophies')
+      .setDescription(`${finalReply}`)
+      .setTimestamp()
+
+    interaction.reply({
+      // content: `${finalReply}`,
+      embeds: [embed]
+    })
+  }
+})
+
+client.login(CLIENT_TOKEN)
---
+++ src/server.ts
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const cors = require('cors')
+require('dotenv').config()
+
+const port = 3000
+
+const apiKey = process.env.BRAWLSTARS_API_TOKEN_KEY
+
+app.use(cors())
+
+app.get('/:playerTag', (req, res) => {
+  const playerTag = req.params.playerTag
+  axios({
+    method: 'get',
+    url: `https://api.brawlstars.com/v1/players/%23${playerTag}`,
+    headers: {
+      'Authorization': `Bearer ${apiKey}`
+    }
+  })
+    .then(response => {
+      // Handle successful response
+      res.send(response.data)
+    })
+    .catch(error => {
+      // Handle error
+      console.log(error)
+      res.send('Player Not Found')
+    })
+})
+
+app.get('/', (req, res) => {
+  res.send('Send Player Tag in Url')
+})
+
+app.listen(port, () => {
+  console.log(`API listening at localhost:${port}`)
+})
